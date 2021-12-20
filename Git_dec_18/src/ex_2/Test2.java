caff4d3138c61b614aaebff3b6679d3c5806ddd5
A1 <A1@LAPTOP-51URUTK2>
2021-12-20 10:15:46 +0530

    2nd

---
+++ Git_dec_18/src/ex_2/Test2.java
@@ -0,0 +1,18 @@
+package ex_2;
+
+public class Test2 
+{
+	public static void main(String[] args) 
+	{
+		Test2 t = new Test2();
+		t.m1();
+		
+		System.out.println("Main Class");
+	}
+	
+	public void m1()
+	{
+		System.out.println("M1 Method");
+	}
+
+}
